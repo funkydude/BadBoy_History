6227a8ac0c30a992ed563a666ca4bcecaaf1305e
funkydude <funkydude@users.noreply.github.com>
2012-05-26 17:47:02 +0000

    Finally make this addon do something, now logs 30 lines of BadBoy history, logging of the other plugins soon (tm).

--- BadBoy_History.lua
+++ BadBoy_History.lua
@@ -1,10 +1,74 @@
 
-BadBoyLogger = function(addon, event, player, msg)
-	event = gsub(event, ".*%_(%L)", "%1")
-	if addon == "BadBoy" then
-		print("|cFF33FF99BadBoy_REPORT|r: ", msg, event)
-	elseif addon == "Guilded" then
-		print("|cFF33FF99BadBoy_GUILDED|r:", msg, event)
+local bbhistory = CreateFrame("Frame", "BadBoyHistory", InterfaceOptionsFramePanelContainer)
+bbhistory:Hide()
+bbhistory.name = "BadBoy_History"
+
+local title = bbhistory:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+title:SetPoint("TOPLEFT", 16, -16)
+title:SetText("BadBoy_History @project-version@") --wowace magic, replaced with tag version
+InterfaceOptions_AddCategory(bbhistory)
+
+local bbhistoryScrollArea = CreateFrame("ScrollFrame", "BadBoyHistoryScroll", bbhistory, "UIPanelScrollFrameTemplate")
+bbhistoryScrollArea:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -15)
+bbhistoryScrollArea:SetPoint("BOTTOMRIGHT", bbhistory, "BOTTOMRIGHT", -30, 10)
+
+local bbhistoryEditBox = CreateFrame("EditBox", "BadBoyHistoryEditBox", bbhistory)
+bbhistoryEditBox:SetMultiLine(true)
+bbhistoryEditBox:SetMaxLetters(99999)
+bbhistoryEditBox:EnableMouse(false)
+bbhistoryEditBox:SetAutoFocus(false)
+bbhistoryEditBox:SetFontObject(ChatFontNormal)
+bbhistoryEditBox:SetWidth(575)
+bbhistoryEditBox:SetHeight(500)
+bbhistoryEditBox:Show()
+
+bbhistoryScrollArea:SetScrollChild(bbhistoryEditBox)
+
+local bbhistoryBackdrop = CreateFrame("Frame", "BadBoyHistoryBackdrop", bbhistory)
+bbhistoryBackdrop:SetBackdrop({bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+	edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+	tile = true, tileSize = 16, edgeSize = 16,
+	insets = {left = 3, right = 3, top = 5, bottom = 3}}
+)
+bbhistoryBackdrop:SetBackdropColor(0,0,0,1)
+bbhistoryBackdrop:SetPoint("TOPLEFT", title, "BOTTOMLEFT", -5, 0)
+bbhistoryBackdrop:SetPoint("BOTTOMRIGHT", bbhistory, "BOTTOMRIGHT", -27, 5)
+
+bbhistory:SetScript("OnShow", function()
+	local text
+	for i=1, #BBHISTORY do
+		if not text then
+			text = BBHISTORY[i]
+		else
+			text = text.."\n\n"..BBHISTORY[i]
+		end
 	end
-end
+	bbhistoryEditBox:SetText(text or "")
+end)
+
+bbhistory:RegisterEvent("PLAYER_LOGIN")
+bbhistory:SetScript("OnEvent", function()
+	if type(BBHISTORY) ~= "table" then
+		BBHISTORY = {}
+	end
+
+	BadBoyLogger = function(addon, event, player, msg)
+		event = (event):sub(10)
+		if addon == "BadBoy" then
+			local BBHISTORY = BBHISTORY
+			local num, dump = #BBHISTORY, "["..BetterDate("%I:%M:%S", time()).."]".."["..event.."]".."["..player.."]: "..msg
+			for i=1, num do
+				if BBHISTORY[i]:find(msg, nil, true) then return end
+			end
+			tinsert(BBHISTORY, dump)
+			bbhistoryEditBox:SetText(bbhistoryEditBox:GetText().. "\n\n"..dump)
+
+			if num > 30 then
+				tremove(BBHISTORY, 1)
+			end
+		elseif addon == "Guilded" then
+			--print("|cFF33FF99BadBoy_GUILDED|r:", msg, event)
+		end
+	end
+end)
 
